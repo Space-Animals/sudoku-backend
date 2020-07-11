de903f538d91204cc81a6bbda9e9330397035a22
deryJ13 <dery.j13@gmail.com>
2020-07-10 23:22:17 -0400

    Added serializer file with User serializer logic

---
+++ sudoku/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models.user import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=300, required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old = serializers.CharField(required=True)
+    new = serializers.CharField(required=True)
